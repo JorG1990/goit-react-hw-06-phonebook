198d3643ba01949acba98e6bdf201c127804e4c7
JorG1990 <jorge  garcia>
2023-09-21 20:16:22 -0500

    add selector.js and store.js

---
+++ src/Redux/Store.js
@@ -0,0 +1,20 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import { contactsReducer } from "./ContactsSlice";
+import { filterReducer } from "./FilterSlice";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistStore } from "redux-persist";
+
+
+export const store = configureStore ({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware({
+    serializableCheck: {
+      ignoreActions: [ FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE,REGISTER],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
