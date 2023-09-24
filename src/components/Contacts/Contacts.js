c23ede97efaa8a3d5fd30a9ef1c28c2517d5675f
JorG1990 <jorge  garcia>
2023-09-24 11:44:57 -0500

    correccion 5

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,35 +1,33 @@
-import { useDispatch, useSelector } from 'react-redux/es/exports';
-import { Table } from './Contacts.styled';
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { Table } from "./Contacts.styled";
 import { deleteContact } from "Redux/contactsSlice";
-import { getContactsList, getFilter } from "Redux/selectors";
+import { getContactsList, getFilter } from "Redux/Selectorsors";
 
 export const Contacts = () => {
-  const dispatch = useDispatch();
-  const contactsList = useSelector(getContactsList);
-  const filterQuery = useSelector(getFilter);
-  const normalizedFilter = filterQuery.toLowerCase();
-  const filteredContacts = contactsList.filter(Contact =>
-    Contact.name.toLowerCase().includes(normalizedFilter)
-  );
-  const deleteContacts = id => {
-    dispatch(deleteContact(id));
-  };
+    const dispatch = useDispatch();
+    const contactsList = useSelector(getContactsList);
+    const filterQuery = useSelector(getFilter);
+    const normalizedFilter = filterQuery.toLowerCase();
+    const filteredContacts = contactsList.filter(Contact => Contact.name.toLowerCase().includes(normalizedFilter));
+    const deleteContacts = id => {
+        dispatch(deleteContact(id));
+    };
 
-  return (
-    <Table>
-      <tbody>
-        {filteredContacts.map(({ id, name, number }) => {
-          return (
-            <tr key={id}>
-              <td>{name}</td>
-              <td>{number}</td>
-              <td>
-                <button onClick={() => deleteContacts(id)}>Delete</button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
-  );
+    return (
+        <Table>
+            <tbody>
+                {filteredContacts.map(({ id, name, number}) => {
+                    return (
+                      <tr key={ id }>
+                          <td>{ name }</td>
+                          <td>{ number }</td>
+                          <td>
+                            <button onClick={() => deleteContacts(id)}>Delete</button>
+                          </td>
+                      </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
+    );
 };
